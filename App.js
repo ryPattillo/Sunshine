087727a3a16d3901d92ad8cddcf732724434dfdc
Ryan Pattillo <ryanpattillo@Ryans-MacBook-Pro-3.local>
2021-10-01 23:02:38 -0400

    Updates

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React,{useState} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import main from './api/power.js';
+import { StyleSheet, Text, View,TextInput,Button } from 'react-native';
+import test from './api/power.js';
 import axios from 'axios';
-//import Geolocation from 'react-native-geolocation-service';
-
-
+import ReactLoading from 'react-loading';
+ 
 
 export default function App() {
 
@@ -15,35 +14,33 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+
+      <Text> Welcome to the Sunshine App </Text>
+         <TextInput
+        placeholder="Input"
+        keyboardType="numeric"
+        />
+        <Button
+        title="test"/>
+         <Button
+        title="test"/>
+         <Button
+        title="test"/>
+         <Button
+        title="test"/>
+    
       <Text>{data}</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
 
-function test(setData) {
 
-  axios.get(
-    "https://power.larc.nasa.gov/api/temporal/hourly/point?start=20210820&end=20210920&latitude=32.123&longitude=90.1243&community=ag&parameters=CLRSKY_SFC_SW_DWN&user=ryan&header=true&time-standard=lst"
-  )
-  .then(function (response) {
-    // handle success
-    console.log(response["data"]["properties"]["parameter"]["CLRSKY_SFC_SW_DWN"]["2021090410"])
-      setData(response["data"]["properties"]["parameter"]["CLRSKY_SFC_SW_DWN"]["2021090410"])
-      //response["data"]["properties"]
-  })
-  .catch(function (error) {
-      // handle error
-      console.log(error);
-  })
-
-}
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 5,
+    height:10,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
 });
--- api/power.js
+++ api/power.js
@@ -1,19 +1,17 @@
 import axios from 'axios';
 
-export default function main () {
-
+export default function test(setData) {
     axios.get(
-        "https://power.larc.nasa.gov/api/temporal/hourly/point?start=20210820&end=20210920&latitude=32.123&longitude=90.1243&community=ag&parameters=CLRSKY_SFC_SW_DWN&user=ryan&header=true&time-standard=lst"
+      "https://power.larc.nasa.gov/api/temporal/hourly/point?start=20210820&end=20210920&latitude=32.123&longitude=90.1243&community=ag&parameters=CLRSKY_SFC_SW_DWN&user=ryan&header=true&time-standard=lst"
     )
     .then(function (response) {
-        // handle success
-        return "test"
+      // handle success
+      console.log(response["data"]["properties"]["parameter"]["CLRSKY_SFC_SW_DWN"]["2021090410"])
+      setData(response["data"]["properties"]["parameter"]["CLRSKY_SFC_SW_DWN"]["2021090410"])
         //response["data"]["properties"]
     })
     .catch(function (error) {
-        // handle error
         console.log(error);
     })
- 
-
-}
+  
+  }
