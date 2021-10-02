89823c2fa5c6c18f5c4fd419e7b8c352ddb39e6d
Ryan Pattillo <ryanpattillo@Ryans-MacBook-Pro-3.local>
2021-10-02 08:31:33 -0400

    Updated App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React,{useState} from 'react';
-import { StyleSheet, Text, View,TextInput,Button,Dimensions  } from 'react-native';
+import { StyleSheet, Text, View,TextInput,Button,Dimensions} from 'react-native';
 import test from './api/power.js';
 import axios from 'axios';
 import ReactLoading from 'react-loading';
@@ -16,16 +16,22 @@ export default function App() {
   const [data,setData] = useState(3)
 
   test(setData)
-
   return (
       <View style={styles.container}>
         <MapView style={styles.map}
+          
           initialRegion={{
             latitude: 37.78825,
             longitude: -122.4324,
             latitudeDelta: 0.0922,
             longitudeDelta: 0.0421,
-          }} />
+          }} 
+          onRegionChange={this.onRegionChange}
+          >
+            <Marker
+  coordinate={{ latitude : initialRegion.latitude , longitude : initialRegion.longitude }}
+/>
+          </MapView>
       </View>
     );
 }
